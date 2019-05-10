cac1d31bca21efc5043af8d59abdd665528d47e3
Aree <aria.j@kakaomobility.com>
2019-05-10 11:44:40 +0900

    initially added main page and edit page

--- app/urls.py
+++ app/urls.py
@@ -3,7 +3,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('', views.main, name='home'),
     path('startups', views.startup_index, name='startup_index'),
     path('startups/<int:id>', views.startup_show, name='startup_show'),
+    path('startups/<int:id>/edit', views.startup_edit, name='startup_edit'),
     path('startups/new', views.startup_new, name='startup_new'),
 ]
