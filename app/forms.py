dacd2e1ee6d6d10aaa221bf7822ddcf239ab31a5
Aree <aria.j@kakaomobility.com>
2019-05-14 13:38:06 +0900

    corrected max lengths of input and added input fields in startup

--- app/forms.py
+++ app/forms.py
@@ -2,18 +2,21 @@ from django import forms
 from jsonfield import JSONField
 from django.core.exceptions import ValidationError
 from django.utils.translation import ugettext_lazy as _
-from .models import Startup, Like
-import app.categories as cate
-# accounts 
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from django import forms
 from django.core.exceptions import ValidationError
 
+from .models import Startup, Like
+import app.categories as cate
+
 class StartupForm(forms.ModelForm):
     class Meta:
         model = Startup
-        fields = ('name', 'location', 'raiseAmount', 'product_name', 'category', 'tags', 'summary')
+        fields = ('name', 'location', 'raiseAmount', 'product_name', 'category',
+                'tags', 'summary', 'background', 'market', 'solution',
+                'business_model', 'future', 'members', 'team_desc', 'timeline',
+                'pitching_video_link', 'product_description')
         widgets = {
             'name': forms.TextInput(attrs = {
                     'placeholder': 'Enter the name of your company or team'
@@ -35,23 +38,21 @@ class StartupForm(forms.ModelForm):
             'summary': forms.Textarea(attrs = {
                     'rows': 3,
                     'placeholder': 'Write what you do and build briefly.'
-                     }),   
+                     }),
         }
         help_texts = {
             'raiseAmount': 'This will be shown in the detail page of your project.',
             'tags': 'Written tags will be used to crawl relative articles for the information and help you to writing a better portfolio.',
-            'product_name': 'This will be shown on the first page. Please write your project name attractively so that VCs want to click.', 
+            'product_name': 'This will be shown on the first page. Please write your project name attractively so that VCs want to click.',
             'category':  'This will decide where your project is shown.',
             'summary': 'This will be shown on the first page. Please write your project name attractively so that VCs want to click.'
         }
         label =  {
             'raiseAmount':'Raise amount',
         }
-        # 'background','market', 'solution', 'business_model', 'future', 'raiseAmount',
-        # 'timeline', 'location', 'summary', 'members', 'team_desc')
 
 
-## 
+##
 class LikeForm(forms.Form):
     user_id = forms.IntegerField()
     startup_id = forms.IntegerField()
@@ -62,15 +63,15 @@ class LikeForm(forms.Form):
         )
         return like
 
-## accounts 
+## accounts
 class CustomUserCreationForm(forms.Form):
-    first_name = forms.CharField(label='Enter your firstname',max_length=150)
-    last_name = forms.CharField(label='Enter your lastname',max_length=150)
-    username = forms.CharField(label='Enter Username', min_length=4, max_length=150)
+    first_name = forms.CharField(label='Enter your firstname',max_length=30)
+    last_name = forms.CharField(label='Enter your lastname',max_length=20)
+    username = forms.CharField(label='Enter Username', min_length=4, max_length=20)
     email = forms.EmailField(label='Enter your email')
     password1 = forms.CharField(label='Enter password', widget=forms.PasswordInput,min_length=8)
     password2 = forms.CharField(label='Confirm password again', widget=forms.PasswordInput,min_length=8)
- 
+
     def clean_first_name(self):
         first_name = self.cleaned_data['first_name'].title()
         return first_name
@@ -85,25 +86,25 @@ class CustomUserCreationForm(forms.Form):
         if r.count():
             raise  ValidationError("Username already exists")
         return username
- 
+
     def clean_email(self):
         email = self.cleaned_data['email'].lower()
         r = User.objects.filter(email=email)
         if r.count():
             raise  ValidationError("Email already exists")
         return email
- 
+
     def clean_password2(self):
         password1 = self.cleaned_data.get('password1')
         password2 = self.cleaned_data.get('password2')
- 
+
         if password1 and password2 and password1 != password2:
             raise ValidationError("Password doesn't match")
 
         if len(password1) < 8:
             raise ValidationError('Password too short')
         return password1
- 
+
     def save(self, commit=True):
         user = User.objects.create_user(
             self.cleaned_data['username'],
