ee6bc79ad527f749381a68fbaff949456a6fc46e
Aree <aria.j@kakaomobility.com>
2019-05-16 05:17:54 +0900

    corrected validator

--- app/validators.py
+++ app/validators.py
@@ -1,30 +1,28 @@
 from django.core.exceptions import ValidationError
-from django.utils.translation import ugettext_lazy as _ 
+from django.utils.translation import ugettext_lazy as _
 from rest_framework import serializers
 import operator
 import json
 
 class TimelineSerializer(serializers.Serializer):
-     date = serializers.CharField(max_length=10)
-     content = serializers.CharField(max_length=100)
+     date = serializers.CharField(max_length=100)
+     content = serializers.CharField(max_length=2000)
 
 class MembersSerializer(serializers.Serializer):
-    name = serializers.CharField(max_length=10)
-    career = serializers.CharField(max_length=100)
+    name = serializers.CharField(max_length=100)
+    career = serializers.CharField(max_length=2000)
 
 def validate_timeline(validated_data):
     serialized = TimelineSerializer(data=validated_data, many=True)
-    if serialized.is_valid():  
+    if serialized.is_valid():
         return validated_data
     else:
         raise ValidationError(_("timeline is not a valid form"))
-        
+
 
 def validate_members(validated_data):
     serialized = MembersSerializer(data=validated_data, many=True)
     if serialized.is_valid():
-        return validated_data 
+        return validated_data
     else:
         raise ValidationError(_("members are not valid "))
-    
-
